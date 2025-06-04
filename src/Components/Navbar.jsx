19c36a627f1b01605990ece2c3c3563e01e7afb2
Dimosthenis Kanellopoulos <dimoskan12@gmail.com>
2025-06-05 01:02:58 +0300

    minor compatibility changes on mobile menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,9 +16,9 @@ export default function Navbar() {
   const navigation = [
   { name: t("menuElement1"), href: '/#essay' },
   { name: t("menuElement2"), href: '/#poem' },
-  { name: t("menuElement3"), href: '/absentminotaur/gallery' },
+  { name: t("menuElement3"), href: '/gallery' },
   { name: t("menuElement4"), href: '/#bio' },
-  { name: t("menuElement5"), href: '/absentminotaur/banner' },
+  { name: t("menuElement5"), href: '/banner' },
 ];
   const location = useLocation();
   const navigate = useNavigate();
@@ -122,18 +122,18 @@ const handleLanguageChange = (lang) => {
       <DisclosurePanel className="sm:hidden">
         <div className="space-y-1 px-2 pt-2 pb-3">
           {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? 'page' : undefined}
-              className={classNames(
-                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium',
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
+        <DisclosureButton
+          key={item.name}
+          as="button"
+          onClick={() => handleNavClick(item.href)}
+          aria-current={item.current ? 'page' : undefined}
+          className={classNames(
+            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block rounded-md px-3 py-2 text-base font-medium',
+          )}
+        >
+          {item.name}
+        </DisclosureButton>
           ))}
         </div>
       </DisclosurePanel>
